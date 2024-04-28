c7c8af75a61161a9dffc70d82bc3a0b82dd8cf90
seongjinOliver <yoosngj@gmail.com>
2024-04-28 15:48:28 +0900

    Add BackJoon15649 - N&M(1)

---
+++ src/main/java/com/algorithm/study/BackJoon15649.java
@@ -0,0 +1,45 @@
+package com.algorithm.study;
+
+import java.util.Scanner;
+
+/**
+ * N과M (1)
+ * https://www.acmicpc.net/problem/15649
+ */
+public class BackJoon15649 {
+
+    public static int[] arr;
+    public static boolean[] visit;
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int M = in.nextInt();
+
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0);
+
+    }
+
+    public static void dfs(int N, int M, int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
